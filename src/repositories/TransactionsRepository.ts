127bf52b194cdcd39e67fb10b1c28739b2771f91
virtualcodemaster <viniciusmarch01@gmail.com>
2020-05-06 01:27:35 -0300

    Projeto 4 finalizado!

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,23 +6,54 @@ interface Balance {
   total: number;
 }
 
+interface TransactionCreate {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
+  private balance: Balance;
+
   constructor() {
     this.transactions = [];
+    this.balance = {
+      income: 0,
+      outcome: 0,
+      total: 0,
+    };
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    if (this.transactions.length > 0) {
+      this.balance.income = 0;
+      this.balance.outcome = 0;
+
+      this.transactions.forEach(transaction => {
+        if (transaction.type === 'income') {
+          this.balance.income += transaction.value;
+        } else if (transaction.type === 'outcome') {
+          this.balance.outcome += transaction.value;
+        }
+      });
+      this.balance.total = this.balance.income - this.balance.outcome;
+    }
+
+    return this.balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: TransactionCreate): Transaction {
+    const transaction = new Transaction({ title, value, type });
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
